ef93f11f0ece6c0019dcf48d7bbf433da98faceb
fauzandp <fauzan27dp@gmail.com>
2023-06-05 14:36:10 +0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Fauzan Dwi Prasetyo on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
