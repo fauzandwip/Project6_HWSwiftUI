406481c3592fc8fa417248e2dd9d5550bb92200e
fauzandp <fauzan27dp@gmail.com>
2023-06-06 15:17:59 +0700

    Project6_SwiftUI | Animations SwiftUI gesture animations

---
+++ Animations/GestureAnimations.swift
@@ -0,0 +1,74 @@
+//
+//  GestureAnimations.swift
+//  Animations
+//
+//  Created by Fauzan Dwi Prasetyo on 05/06/23.
+//
+
+import SwiftUI
+
+struct GestureAnimationsView: View {
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var dragAmount = CGSize.zero
+    @State private var enabled = false
+    
+    var body: some View {
+        
+        // MARK: - First Content
+        
+        VStack {
+            LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .frame(width: 300, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .offset(dragAmount)
+                .gesture(
+                    DragGesture()
+                        .onChanged { dragAmount = $0.translation }
+                        .onEnded { _ in
+                            // explicit animation
+                            withAnimation(.spring()) {
+                                dragAmount = .zero
+                            }
+                        }
+                )
+            // implicit animation
+            //            .animation(.spring(), value: dragAmount)
+        }
+        
+        
+        // MARK: - Second Content
+        
+        VStack {
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count, id: \.self) { num in
+                    Text(String(letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        // 5. background triggered, background changed
+                        .background(enabled ? .blue : .red)
+                        // 2. offset() triggered, view moving
+                        .offset(dragAmount)
+                        // 3. animation() triggered while moving with offset, with delay
+                        .animation(.default.delay(Double(num) / 20), value: dragAmount)
+                }
+            }
+            .gesture(
+                DragGesture()
+                    // 1. while dragged, dragAmount changed
+                    .onChanged { dragAmount = $0.translation }
+                    // 4. while user lifts their finger, back to .zero position
+                    .onEnded { _ in
+                        dragAmount = .zero
+                        enabled.toggle()
+                    }
+            )
+        }
+    }
+}
+
+struct GestureAnimationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        GestureAnimationsView()
+    }
+}
