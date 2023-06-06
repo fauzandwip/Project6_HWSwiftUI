face7402dfe29e20d8355f5edf58cbed5f794fda
fauzandp <fauzan27dp@gmail.com>
2023-06-06 15:11:09 +0700

    Project6_SwiftUI | Animations SwiftUI create implicit animations customizing animation in swiftUI

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Tap Me") {
+//            animationAmount += 1
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//        .animation(.default, value: animationAmount)
+//        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
+//        .animation(.easeInOut(duration: 2), value: animationAmount)
+//        .animation(
+//            .easeInOut(duration: 2)
+//            .delay(1),
+//            value: animationAmount)
+//        .animation(
+//            .easeInOut(duration: 2)
+//            .repeatCount(3, autoreverses: true),
+//            value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.black)
+//                .fill(.mint)
+                .background(.mint)
+                .clipShape(Circle())
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 2)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount)
+        )
+        .onAppear{
+//            animationAmount = 2
+        }
     }
 }
 
